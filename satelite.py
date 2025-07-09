9bc4e950d6f4637adba86fff61af906cab0bfc30
UjasDubal <148449626+UjasDubal@users.noreply.github.com>
2025-07-09 22:19:50 +0530

    Update satelite.py

--- satelite.py
+++ satelite.py
@@ -2,12 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from PIL import Image
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+
 import os
 
 # Set page configuration
